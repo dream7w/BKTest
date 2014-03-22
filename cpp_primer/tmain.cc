a17c2b3db7365273b91127f16ae7fcb4207abda9
dream7w <dream7w@hotmail.com>
2014-03-22 00:22:21 -0700

    01_01

--- cpp_primer/tmain.cc
+++ cpp_primer/tmain.cc
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "gtest/gtest.h"
 
-int main()
+class GTTest : public ::testing::Test
 {
-  return 0;
+  protected:
+    static void SetUpTestCase(){}
+    static void TearDownTestCase(){}
+
+    virtual void SetUp(){}
+    virtual void TearDown(){}
+};
+
+TEST_F(GTTest, Foo)
+{
+  ASSERT_TRUE(1 == 1);
+}
+
+int main(int argc, char **argv)
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
 }
