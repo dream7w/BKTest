9fcbe63374fbc16d09263b86ce091730bb0b41f5
zdfcumt <zdfcumt@yeah.net>
2014-06-11 23:25:23 -0700

    InsertSort

---
+++ i2algorithms/src/chp02.cc
@@ -0,0 +1,16 @@
+/*==========================================
+  @Author      : zabambo
+  @File        : chp02.cc
+  @Encoding    : utf-8
+  @Create      : 2014-06-11 23:07:34
+  @Modified    : 2014-06-11 23:08:52
+  @Description : 
+==========================================*/
+
+#include "chp02.h"
+
+
+void InsertSort()
+{
+
+}
---
+++ i2algorithms/src/chp02.h
@@ -0,0 +1,18 @@
+/*==========================================
+  @Author      : zabambo
+  @File        : chp02.h
+  @Encoding    : utf-8
+  @Create      : 2014-06-11 23:07:22
+  @Modified    : 2014-06-11 23:12:00
+  @Description : 
+==========================================*/
+
+/*
+ *  brief   插入排序,对少量元素进行排序的有效算法
+ *  input
+ *  output
+ */
+void InsertSort();
+
+
+
