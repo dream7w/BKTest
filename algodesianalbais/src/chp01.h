65e06b0c0518cbfc67fd0f3bce4f809ef25cfacf
zdfcumt <zdfcumt@yeah.net>
2014-07-07 21:05:53 +0800

    chp01

---
+++ algodesianalbais/src/chp01.h
@@ -0,0 +1,16 @@
+/*==========================================
+  @Author      : zabambo
+  @File        : src/chp01.h
+  @Encoding    : utf-8
+  @Create      : 2014-07-07 20:14:00
+  @Modified    : 2014-07-07 20:15:54
+  @Description : 
+==========================================*/
+
+#ifndef SRC_CHP01_H_
+#define SRC_CHP01_H_
+
+//欧几里得最大公约数
+int gcd(int a, int b);
+
+#endif//SRC_CHP01_H_
---
+++ algodesianalbais/src/test.cc
@@ -0,0 +1,17 @@
+/*==========================================
+  @Author      : zabambo
+  @File        : algodesianalbais/src/test.cc
+  @Encoding    : utf-8
+  @Create      : 2014-07-05 10:01:23
+  @Modified    : 2014-07-05 10:02:08
+  @Description : 
+==========================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
