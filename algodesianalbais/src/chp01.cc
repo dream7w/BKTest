f858306e95fbae12f12c39482149759f1175a865
zdfcumt <zdfcumt@yeah.net>
2014-07-08 15:34:34 +0800

    欧几里得最大公约数 减法

--- algodesianalbais/src/chp01.cc
+++ algodesianalbais/src/chp01.cc
@@ -3,24 +3,48 @@
   @File        : src/chp01.cc
   @Encoding    : utf-8
   @Create      : 2014-07-07 20:13:58
-  @Modified    : 2014-07-08 14:57:07
+  @Modified    : 2014-07-08 15:32:33
   @Description : 
 ==========================================*/
 #include "chp01.h"
 
 void TestCHP01()
 {
-  printf("%d\n", gcd(1000,100));
+  printf("%d\n", gcd(1000,499));
+  printf("%d\n", gcdsub(1000,499));
 }
 
 int gcd(int a, int b)
 {
-  printf("a:%d, b:%d\n", a, b);
   int c = 0;
   while((c = a % b) > 0){
     a = b; 
     b = c;
-    printf("a:%d, b:%d\n", a, b);
   }
   return b;
 }
+
+int gcdsub(int a, int b)
+{
+  int c = a;
+  while(c){
+    if (a < b){
+      c = a;
+      a = b;
+      b = c;
+    }
+    c = a - b;
+    a = b;
+    b = c;
+  }
+  return a;
+}
+
+bool GcdGame(int a, int b)
+{
+  int c = gcd(a, b);
+  int cnt = a / c;
+  return (cnt % 2 > 0);
+}
+
+
