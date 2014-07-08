59400aa4087366d286c166394e7ecff7c737f1a2
zdfcumt <zdfcumt@yeah.net>
2014-07-08 17:11:32 +0800

    扩展欧几里得算法

--- algodesianalbais/src/chp01.cc
+++ algodesianalbais/src/chp01.cc
@@ -3,20 +3,29 @@
   @File        : src/chp01.cc
   @Encoding    : utf-8
   @Create      : 2014-07-07 20:13:58
-  @Modified    : 2014-07-08 15:44:38
+  @Modified    : 2014-07-08 16:53:18
   @Description : 
 ==========================================*/
 #include "chp01.h"
 
 void TestCHP01()
 {
-  printf("%d\n", gcd(1000,499));
-  printf("%d\n", gcdsub(1000,499));
+  printf("%d\n", Gcd(1000,499));
+  printf("%d\n", GcdSub(1000,499));
+  int a=3,b=2,d=0,x,y;
+  ExtGcdRecu(a, b, d, x, y);
+  printf("a:%d, b:%d, d:%d, x:%d, y:%d\n", a, b, d, x, y);
+  
 }
 
-int gcd(int a, int b)
+int Gcd(int a, int b)
 {
   int c = 0;
+  if (a < b){
+    c = a;
+    a = b;
+    b = c;
+  }
   while(b){
     c = a % b;
     a = b; 
@@ -25,7 +34,7 @@ int gcd(int a, int b)
   return a;
 }
 
-int gcdsub(int a, int b)
+int GcdSub(int a, int b)
 {
   int c = a;
   while(b){
@@ -43,9 +52,27 @@ int gcdsub(int a, int b)
 
 bool GcdGame(int a, int b)
 {
-  int c = gcd(a, b);
+  int c = Gcd(a, b);
   int cnt = a / c;
   return (cnt % 2 > 0);
 }
 
+void ExtGcdRecu(int a, int b, int &d, int &x, int &y)
+{
+  if (0 == b){
+    x = 1;
+    y = 0;
+    d = a;
+    return;
+  }
+  ExtGcdRecu(b, a % b, d, x, y);
+  int t = x;
+  x = y;
+  y = t - a/b * y;
+}
+
+void ExtGcdNoRecu(int a, int b, int &d, int &x, int &y)
+{
+  
+}
 
--- algodesianalbais/src/chp01.h
+++ algodesianalbais/src/chp01.h
@@ -3,7 +3,7 @@
   @File        : src/chp01.h
   @Encoding    : utf-8
   @Create      : 2014-07-07 20:14:00
-  @Modified    : 2014-07-08 15:33:46
+  @Modified    : 2014-07-08 17:11:06
   @Description : 
 ==========================================*/
 
@@ -15,9 +15,9 @@
 
 
 //欧几里得最大公约数---求模法
-int gcd(int a, int b);
+int Gcd(int a, int b);
 //欧几里得最大公约数---减法
-int gcdsub(int a, int b);
+int GcdSub(int a, int b);
 
 /*
 欧几里得游戏:
@@ -34,6 +34,27 @@ int gcdsub(int a, int b);
 //返回true先行动
 bool GcdGame(int a, int b);
 
+/*
+扩展欧几里得算法
+对于不完全为 0 的非负整数 a，b，d表示 a，b 的最大公约数，
+必然存在整数对 x，y ，使得 d=ax+by。
+
+证明：设 a>b。
+1，显然当 b=0，gcd（a，b）=a。此时 x=1，y=0；
+2，ab!=0 时
+　　设 ax1+by1=gcd(a,b);
+　　bx2+(a mod b)y2=gcd(b,a mod b);
+　　根据朴素的欧几里德原理有 gcd(a,b)=gcd(b,a mod b);
+　　则:ax1+by1=bx2+(a mod b)y2;
+　　即:ax1+by1=bx2+(a-(a/b)*b)y2=ay2+bx2-(a/b)*by2;
+　　根据恒等定理得：x1=y2; y1=x2-(a/b)*y2;
+     这样我们就得到了求解 x1,y1 的方法：x1，y1 的值基于 x2，y2.
+　  上面的思想是以递归定义的，因为 gcd 不断的递归求解一定会有个时候 b=0，所以递归可以结束。
+*/
+//递归算法，完成
+void ExtGcdRecu(int a, int b, int &d, int &x, int &y);
+//非递归算法，未完成
+void ExtGcdNoRecu(int a, int b, int &d, int &x, int &y);
 
 
 void TestCHP01();
