f8177c48227c55f4591157c93789c0bda91d6c48
zdfcumt <zdfcumt@yeah.net>
2014-07-10 14:47:09 +0800

    chp09

---
+++ datastrualgoanal/src/chp09.h
@@ -0,0 +1,20 @@
+/*==========================================
+  @Author      : zabambo
+  @File        : datastrualgoanal/src/chp09.h
+  @Encoding    : utf-8
+  @Create      : 2014-07-10 14:45:57
+  @Modified    : 2014-07-10 14:46:41
+  @Description : 
+==========================================*/
+
+#ifndef SRC_CHP09_H_
+#define SRC_CHP09_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void TestCHP09();
+
+#endif//SRC_CHP09_H_
--- datastrualgoanal/src/test.cc
+++ datastrualgoanal/src/test.cc
@@ -1,19 +1,21 @@
 /*==========================================
   @Author      : zabambo
-  @File        : algodesianalbais/src/test.cc
+  @File        : datastrualgoanal/src/test.cc
   @Encoding    : utf-8
-  @Create      : 2014-07-05 10:01:23
-  @Modified    : 2014-07-08 11:45:27
+  @Create      : 2014-07-10 14:45:34
+  @Modified    : 2014-07-10 14:46:54
   @Description : 
 ==========================================*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "chp01.h"
+#include "chp09.h"
 
 
 int main(int argc, char **argv)
 {
-  TestCHP01();
+  //TestCHP01();
+  TestCHP09();
   return 0;
 }
