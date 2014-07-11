b92f856f2702c098e5e34e16e600267ee63b8038
zdfcumt <zdfcumt@yeah.net>
2014-07-11 14:03:05 +0800

    Top Sort

--- datastrualgoanal/src/chp09.cc
+++ datastrualgoanal/src/chp09.cc
@@ -3,7 +3,7 @@
   @File        : datastrualgoanal/src/chp09.cc
   @Encoding    : utf-8
   @Create      : 2014-07-10 14:45:48
-  @Modified    : 2014-07-10 14:46:36
+  @Modified    : 2014-07-11 14:02:46
   @Description : 
 ==========================================*/
 
@@ -12,4 +12,111 @@
 void TestCHP09()
 {
 }
+
+GraphArray::GraphArray()
+  :v(0), e(0), data(NULL)
+{
+}
+
+GraphArray::~GraphArray()
+{
+
+}
+
+
+GraphArray::GraphArray(int v)
+  :v(v), e(0), data(NULL)
+{
+  data = (int**)malloc(sizeof(int*) * v);
+  for (int i = 0; i < v; ++i){
+    data[i] = new int[v];
+  }
+}
+
+bool GraphArray::TopSort(std::vector<int> &out)
+{
+  std::vector<int> vec_indegree;
+  vec_indegree.resize(v);
+  for (int i = 0; i < v; ++i){
+    for (int j = 0; j < v; ++j){
+      if (data[i][j] > 0){
+        vec_indegree[j] += 1;
+      }
+    }
+  }
+
+  std::deque<int> zero_queue;
+  for (int i = 0; i < v; ++i){
+    if (vec_indegree[i] == 0){
+      zero_queue.push_back(i);
+      --vec_indegree[i];
+    }
+  }
+  while(!zero_queue.empty()){
+    int id = zero_queue.front();
+    zero_queue.pop_front();
+    out.push_back((int)id);
+    for (int i = 0; i < v; ++i){
+      if (data[id][i] > 0){
+        vec_indegree[i] -= 1;
+        if (vec_indegree[i] == 0){
+          zero_queue.push_back(i);
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+
+GraphList::GraphList()
+  :v(0), e(0), data(NULL)
+{
+}
+
+GraphList::~GraphList()
+{
+
+}
+
+GraphList::GraphList(int v)
+  :v(0), e(v), data(NULL)
+{
+    data = new std::list<int>[v];
+};
+
+bool GraphList::TopSort(std::vector<int> &out)
+{
+  std::deque<int> zero_queue;
+  std::vector<int> vec_indegree;
+  vec_indegree.resize(v);
+  for (int i = 0; i < v; ++i){
+    for (std::list<int>::iterator it = data[i].begin(); it != data[i].end(); ++it){
+      vec_indegree[*it] += 1;
+    }
+  }
+
+  for (int i = 0; i < v; ++i){
+    if (vec_indegree[i] == 0){
+      zero_queue.push_back(i);
+      --vec_indegree[i];
+    }
+  }
+  while(!zero_queue.empty()){
+    int id = zero_queue.front();
+    zero_queue.pop_front();
+    out.push_back((int)id);
+    for (std::list<int>::iterator it = data[id].begin(); it != data[id].end(); ++it){
+      vec_indegree[*it] -= 1;
+      if (vec_indegree[*it] == 0){
+        zero_queue.push_back(*it);
+      }
+    }
+  }
+
+  return true;
+}
+
+
 
--- datastrualgoanal/src/chp09.h
+++ datastrualgoanal/src/chp09.h
@@ -3,7 +3,7 @@
   @File        : datastrualgoanal/src/chp09.h
   @Encoding    : utf-8
   @Create      : 2014-07-10 14:45:57
-  @Modified    : 2014-07-10 14:46:41
+  @Modified    : 2014-07-11 14:00:27
   @Description : 
 ==========================================*/
 
@@ -12,6 +12,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <list>
+#include <vector>
+#include <deque>
+
+
+struct GraphInterface
+{
+  virtual bool TopSort(std::vector<int> &out) = 0;
+};
+
+struct GraphArray : public GraphInterface
+{
+  int v;  //顶点数
+  int e;  //边数
+  int **data;
+
+  GraphArray();
+  virtual ~GraphArray();
+  GraphArray(int e);
+  virtual bool TopSort(std::vector<int> &out);
+};
+
+
+struct GraphList : public GraphInterface
+{
+  int v;  //顶点数
+  int e;  //边数
+  std::list<int> *data;
+
+  GraphList();
+  virtual ~GraphList();
+  GraphList(int e);
+  virtual bool TopSort(std::vector<int> &out);
+};
+
+
+
 
 
 
